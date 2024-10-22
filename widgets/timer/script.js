2abd9941fcacc076c6c5248481ff1adedb2691da
Bonza2710 <159284630+Bonza2710@users.noreply.github.com>
2024-10-22 21:29:52 +0100

    gave timer leading zeros

--- widgets/timer/script.js
+++ widgets/timer/script.js
@@ -1,6 +1,18 @@
 // Set the date you're counting down to (e.g., January 1, 2025)
 const countdownDate = new Date("Dec 13, 2024 18:00:00").getTime();
 
+//add leading zero
+function zeroPad(num, numZeros) {
+  var n = Math.abs(num);
+  var zeros = Math.max(0, numZeros - Math.floor(n).toString().length );
+  var zeroString = Math.pow(10,zeros).toString().substring(1);
+  if( num < 0 ) {
+      zeroString = '-' + zeroString;
+  }
+
+  return zeroString+n;
+}
+
 // Update the countdown every second
 const countdownFunction = setInterval(function () {
   // Get today's date and time
@@ -19,11 +31,11 @@ const countdownFunction = setInterval(function () {
   const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
   // Output the result in the respective elements
-  document.getElementById("weeks").innerHTML = weeks
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("weeks").innerHTML = zeroPad(weeks, 2)
+  document.getElementById("days").innerHTML = zeroPad(days, 2);
+  document.getElementById("hours").innerHTML = zeroPad(hours, 2);
+  document.getElementById("minutes").innerHTML = zeroPad(minutes, 2);
+  document.getElementById("seconds").innerHTML = zeroPad(seconds, 2);
 
   // If the countdown is over, display a message
   if (timeLeft < 0) {
